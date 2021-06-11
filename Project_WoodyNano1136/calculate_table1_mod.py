bdb7ea2eca58e58032d9ee57d0320c4f56e86298
Jerry Chu <jerry955071@gmail.com>
2021-06-11 17:14:00 +0800

    add group "total"

--- Project_WoodyNano1136/calculate_table1_mod.py
+++ Project_WoodyNano1136/calculate_table1_mod.py
@@ -10,12 +10,13 @@ path_table1 = sys.argv[2]
 df = pd.read_csv(csv_path)
 # %%
 c = df['read_length_woodynano'] - df['read_length_pychopper']
+cond0 = np.array([True]*df.shape[0])
 cond1 = (c >= min(c)) & (c < 0)  # D		96.6880962193937
 cond2 = c == 0  # C						0.26052399618539923
 cond3 = (c > 0) & (c < -min(c))  # B		1.9632558881130664
 cond4 = c >= -min(c)  # A 				1.0881238963078395
 
-for cond, group in zip([cond1, cond2, cond3, cond4], ['D', 'C', 'B', 'A']):
+for cond, group in zip([cond0, cond1, cond2, cond3, cond4], ['total', 'D', 'C', 'B', 'A']):
 
     stats = ['read_length','mapped_read_length','matching_nucleotides','softclipped_nucleotides']
     table1 = {
@@ -30,7 +31,7 @@ for cond, group in zip([cond1, cond2, cond3, cond4], ['D', 'C', 'B', 'A']):
         mean_p = np.mean(df[cond][f'{stat}_pychopper'])
         diff = mean_w - mean_p
         diff_per = diff/mean_w*100
-        
+
         table1['Statistic'].append(stat)
         table1['Woodynano'].append('%.1f' % mean_w)
         table1['Pychopper'].append('%.1f' % mean_p)
