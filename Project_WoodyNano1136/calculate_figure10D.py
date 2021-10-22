b56bdd91c703c5e90cefda290caefe08fb1debd7
jerry955071 <61039702+jerry955071@users.noreply.github.com>
2021-10-22 13:27:18 +0800

    now take 2in1.csv as input

--- Project_WoodyNano1136/calculate_figure10D.py
+++ Project_WoodyNano1136/calculate_figure10D.py
@@ -1,24 +1,35 @@
-# %%
 import pandas as pd
 import numpy as np
 import sys
-#%%
-# csv_path = '/Users/zhujiachen/Desktop/WoodyNano_Revision/Stats/Summarized.csv'
+
+# Usage:
+# python [scirpt] [2in1.csv] > [output]
+
+# [2in1.csv] file header:
+# readname,read_length_woodynano,read_length_pychopper,
+# mapped_read_length_woodynano,mapped_read_length_pychopper,
+# matching_nucleotides_woodynano,matching_nucleotides_pychopper,
+# softclipped_nucleotides_woodynano,softclipped_nucleotides_pychopper,
+# clipping_left_woodynano,clipping_left_pychopper,
+# clipping_right_woodynano,clipping_right_pychopper,
+# ref_start_woodynano,ref_start_pychopper,
+# ref_end_woodynano,ref_end_pychopper,
+# numbers_of_exon_woodynano,numbers_of_exon_pychopper
+
 csv_path = sys.argv[1]
 df = pd.read_csv(csv_path, index_col=0)
 
-a = np.array(df[df['software'] == 'woodynano']['read_length'])
-b = np.array(df[df['software'] == 'pychopper']['read_length'])
+a = np.array(df["read_length_woodynano"])
+b = np.array(df["read_length_pychopper"])
 c = a - b
-# %%
 
 cond1 = (c >= min(c)) & (c < 0)
 
-tmp1 = df[df['software'] == 'woodynano'][cond1]
-tmp2 = df[df['software'] == 'pychopper'][cond1]
+# tmp1 = df["read_length_woodynano"][cond1]
+# tmp2 = df["read_length_pychopper"][cond1]
 
-pychopper_residual = np.array(tmp2['read_length']) - np.array(tmp1['read_length'])
-pychopper_unmapped_residual = np.array(tmp2['softclipped_nucleotides']) - np.array(tmp1['softclipped_nucleotides'])
+pychopper_residual = np.array(df["read_length_pychopper"][cond1]) - np.array(df["read_length_woodynano"][cond1])
+pychopper_unmapped_residual = np.array(df["softclipped_nucleotides_pychopper"][cond1]) - np.array(df['softclipped_nucleotides_woodynano'][cond1])
 
 mapped_rate = 1 - sum(pychopper_unmapped_residual) / sum(pychopper_residual)
 
