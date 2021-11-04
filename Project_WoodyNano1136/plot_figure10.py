70728716cf509a8ae6f3f75db2afacad330cc3dc
jerry955071 <61039702+jerry955071@users.noreply.github.com>
2021-11-04 21:50:11 +0800

    Update plot_figure10.py

--- Project_WoodyNano1136/plot_figure10.py
+++ Project_WoodyNano1136/plot_figure10.py
@@ -6,8 +6,14 @@ import sys
 #%%
 csv_path = sys.argv[1]
 figure10_path = sys.argv[2]
+colors = sys.argv[3:]
 
-def Figure10_modified(data, path, title, lims=[0, 6000]):
+def Figure10_modified(data, path, title, colors, lims=[0, 6000]):
+    samples = set(list(data["Species_bio"]))
+    color_palette = {}
+    for s, c in zip(samples, colors):
+        color_palette[samples] = colors
+        
     sns.set_theme(
         context='paper',
         font_scale=1.2,
@@ -21,12 +27,7 @@ def Figure10_modified(data, path, title, lims=[0, 6000]):
                               y='Woodynano',
                               hue='Species_bio',
                               legend=False,
-                              palette={'Ptr_bio1': 'darkorange',
-                                       'Ptr_bio2': 'orange',
-                                       'Egr_bio1': 'blue',
-                                       'Egr_bio2': 'dodgerblue',
-                                       'Lch_bio1': 'green',
-                                       'Lch_bio2': 'limegreen'},
+                              palette=color_palette,
                               s=2,
                               ax=ax1)
 
@@ -40,12 +41,7 @@ def Figure10_modified(data, path, title, lims=[0, 6000]):
     snsplt2 = sns.histplot(x=data['Woodynano']-data['Pychopper'],
                            hue=data['Species_bio'],
                            ax=ax2,
-                           palette={'Ptr_bio1': 'darkorange',
-                                    'Ptr_bio2': 'orange',
-                                    'Egr_bio1': 'blue',
-                                    'Egr_bio2': 'dodgerblue',
-                                    'Lch_bio1': 'green',
-                                    'Lch_bio2': 'limegreen'},
+                           palette=color_palette,
                            fill=False,
                            legend=False,
                            element='poly',
@@ -57,24 +53,24 @@ def Figure10_modified(data, path, title, lims=[0, 6000]):
     return
 
 
-path_summarized_csv = csv_path
-tmp = pd.read_csv(path_summarized_csv)
+tmp = pd.read_csv(csv_path)
 
 reshaped_df = tmp[['bioname', 'read_length_woodynano', 'read_length_pychopper']]
-slice_1 = reshaped_df[reshaped_df['bioname'] == 'Egr_bio1']
-slice_2 = reshaped_df[reshaped_df['bioname'] == 'Egr_bio2']
-slice_3 = reshaped_df[reshaped_df['bioname'] == 'Ptr_bio1']
-slice_4 = reshaped_df[reshaped_df['bioname'] == 'Ptr_bio2']
-slice_5 = reshaped_df[reshaped_df['bioname'] == 'Lch_bio1']
-slice_6 = reshaped_df[reshaped_df['bioname'] == 'Lch_bio2']
+# slice_1 = reshaped_df[reshaped_df['bioname'] == 'Egr_bio1']
+# slice_2 = reshaped_df[reshaped_df['bioname'] == 'Egr_bio2']
+# slice_3 = reshaped_df[reshaped_df['bioname'] == 'Ptr_bio1']
+# slice_4 = reshaped_df[reshaped_df['bioname'] == 'Ptr_bio2']
+# slice_5 = reshaped_df[reshaped_df['bioname'] == 'Lch_bio1']
+# slice_6 = reshaped_df[reshaped_df['bioname'] == 'Lch_bio2']
 
-sorted_df = pd.concat([slice_1, slice_2, slice_3, slice_4, slice_5, slice_6])
-sorted_df.columns = ['Species_bio', 'Woodynano', 'Pychopper']
+# sorted_df = pd.concat([slice_1, slice_2, slice_3, slice_4, slice_5, slice_6])
+reshaped_df.columns = ['Species_bio', 'Woodynano', 'Pychopper']
 
 #%%
 Figure10_modified(
-    data=sorted_df,
+    data=reshaped_df,
     path=figure10_path,
+    colors=colors
     title=''
 )
 #%%
